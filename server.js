f432fadc6e6e98df3448edbad5e65f262f53212f
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-10-14 21:54:12 +0200

    fixed cors issue

--- server.js
+++ server.js
@@ -28,16 +28,14 @@ const app = express();
 // Trust proxy configuration
 //app.set('trust proxy', 1);
 
-// CORS configuration
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    maxAge: 86400
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+    origin : (origin, callback) => {
+        return callback(null, true)
+    },
+    methods : ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders : ["Content-Type", "Authorization"],
+    credentials : true
+}));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
